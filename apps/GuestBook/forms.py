25068e698e68dd8c40ac5dfe833614bcb305e222
limodou.project <limodou.project@14287918-b64d-0410-abb6-a92efa0c2026>
2008-06-12 15:05:24 +0000

    update GuestBook app, make it look better

--- apps/GuestBook/forms.py
+++ apps/GuestBook/forms.py
@@ -1,5 +1,7 @@
 from utils import Form
 
+Form.Form.layout_class = Form.CSSLayout
+
 class NoteForm(Form.Form):
     message = Form.TextAreaField(label='Message:', required=True)
     username = Form.TextField(label='Username:', required=True)
