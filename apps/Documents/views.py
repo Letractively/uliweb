b363dec5a85cbee76cb6bda84f4ef0fc9c00b66a
limodou.project <limodou.project@14287918-b64d-0410-abb6-a92efa0c2026>
2008-06-22 02:50:58 +0000

    add language switch function and add template function in Dispatcher

--- apps/Documents/views.py
+++ apps/Documents/views.py
@@ -7,20 +7,23 @@ def __begin__():
 
 @expose('/documents')
 def documents():
-    return _show(request, 'content.rst', env)
+    return _show(request, response, 'content.rst', env)
 
-def _show(request, filename, env, lang=None, render=True):
+def _show(request, response, filename, env, lang=None, render=True):
     from uliweb.core.template import template
     from uliweb.utils.rst import to_html
-    from uliweb.i18n import get_language
+    from uliweb.i18n import get_language, format_locale
     if not filename.endswith('.rst'):
         filename += '.rst'
     if not lang:
-        #get from query string
+        #get from query string, and auto set_cookie
         lang = request.GET.get('lang')
         if not lang:
             lang = get_language()
+        else:
+            response.set_cookie(env.config.get('LANGUAGE_COOKIE_NAME'), lang)
     if lang:
+        lang = format_locale(lang)
         f = env.get_file(os.path.join(lang, filename))
         if f:
             filename = f
@@ -29,7 +32,8 @@ def _show(request, filename, env, lang=None, render=True):
         content = to_html(template(content, env=env))
     else:
         content = to_html(content)
-    return locals()
+    response.write(application.template('show_document.html', locals()))
+    return response
     
 @expose('/documents/<regex(".*$"):filename>', defaults={'lang':''})
 #@expose('/documents/<regex(".*$"):filename>')
@@ -37,4 +41,4 @@ def _show(request, filename, env, lang=None, render=True):
 #this is also available
 @expose('/documents/<lang>/<regex(".*$"):filename>')
 def show_document(filename, lang):
-    return _show(request, filename, env, lang, False)
+    return _show(request, response, filename, env, lang, False)
