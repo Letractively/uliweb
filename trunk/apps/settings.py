fd2910a0a4134e2f203ecc6e98a9bdf0bb511d37
limodou.project <limodou.project@14287918-b64d-0410-abb6-a92efa0c2026>
2008-11-15 09:55:28 +0000

    move helpers from uliweb folder to lib folder

--- trunk/apps/settings.py
+++ trunk/apps/settings.py
@@ -1,4 +1,4 @@
 INSTALLED_APPS = ['Documents', 'Examples', 'Portal', 'Post', 
-    'uliweb.builtins.auth', 'uliweb.helpers.htmlwidgets']
+    'uliweb.builtins.auth', 'helpers.htmlwidgets']
 DEBUG = False
 #INSTALLED_APPS = ['Hello'] 
