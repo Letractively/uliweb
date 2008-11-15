7affd24121bf839f0be4be6876396d00a3f70ac5
limodou.project <limodou.project@14287918-b64d-0410-abb6-a92efa0c2026>
2008-11-15 12:48:10 +0000

    add jxlib http://jxlib.org/

--- trunk/apps/Portal/settings.py
+++ trunk/apps/Portal/settings.py
@@ -82,30 +82,15 @@ def startup(sender):
 ##################################################
 @plugin('before_render_template')
 def before_render_template(sender, env, out):
-    from uliweb.core import js
+    import helpers
     from uliweb.core.SimpleFrame import url_for
     from helpers import htmlwidgets
     
-    htmlbuf = js.HtmlBuf(write=out.noescape, static_suffix=url_for('Portal.views.static', filename=''))
-    env['htmlbuf'] = htmlbuf
-    env['htmlwidgets'] = htmlwidgets
+    helpers.before_render_template(env, out.noescape, url_for('Portal.views.static', filename=''), 
+        htmlwidgets=htmlwidgets)
     
 @plugin('after_render_template')
 def after_render_template(sender, text, vars, env):
-    import re
-    r_links = re.compile('<link\s.*?\shref\s*=\s*"?(.*?)["\s>]|<script\s.*?\ssrc\s*=\s*"?(.*?)["\s>]', re.I)
-    if 'htmlbuf' in env:
-        htmlbuf = env['htmlbuf']
-        if htmlbuf.modified:
-            b = re.search('(?i)</head>', text)
-            if b:
-                pos = b.start()
-                #find links
-                links = [x or y for x, y in r_links.findall(text[:pos])]
-                htmlbuf.remove_links(links)
-                t = htmlbuf.render()
-                if t:
-                    return ''.join([text[:pos], t, text[pos:]])
-            else:
-                return t+text
-    return text
+    import helpers
+    
+    return helpers.after_render_template(text, vars, env)
---
+++ trunk/lib/helpers/__init__.py
@@ -0,0 +1,28 @@
+def before_render_template(env, writer, static_suffix, **widgets):
+    from uliweb.core import js
+    from uliweb.core.SimpleFrame import url_for
+    from helpers import htmlwidgets
+
+    htmlbuf = js.HtmlBuf(write=writer, static_suffix=static_suffix)
+    env['htmlbuf'] = htmlbuf
+    for k, v in widgets.items():
+        env[k] = v
+
+def after_render_template(text, vars, env):
+    import re
+    r_links = re.compile('<link\s.*?\shref\s*=\s*"?(.*?)["\s>]|<script\s.*?\ssrc\s*=\s*"?(.*?)["\s>]', re.I)
+    if 'htmlbuf' in env:
+        htmlbuf = env['htmlbuf']
+        if htmlbuf.modified:
+            b = re.search('(?i)</head>', text)
+            if b:
+                pos = b.start()
+                #find links
+                links = [x or y for x, y in r_links.findall(text[:pos])]
+                htmlbuf.remove_links(links)
+                t = htmlbuf.render()
+                if t:
+                    return ''.join([text[:pos], t, text[pos:]])
+            else:
+                return t+text
+    return text
