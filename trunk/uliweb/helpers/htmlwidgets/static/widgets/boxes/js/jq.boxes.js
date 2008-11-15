f74466b6ae06e3dab0da42367e820b34979d0c40
limodou.project <limodou.project@14287918-b64d-0410-abb6-a92efa0c2026>
2008-11-15 09:55:07 +0000

    move helpers from uliweb folder to lib folder

--- trunk/uliweb/helpers/htmlwidgets/static/widgets/boxes/js/jq.boxes.js
+++
@@ -1,8 +0,0 @@
-(function($){
-	$.fn.boxes = function(){
-		return this.each(function(){
-			var t = $(this);
-			t.wrap('<div class="cssbox"><div class="c1"><div class="c2"></div></div><div class="c3"></div></div>');
-		});
-	};
-})(jQuery);
