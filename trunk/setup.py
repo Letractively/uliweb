6c389e1e9e832f39b387ae346419106066c30dab
limodou.project <limodou.project@14287918-b64d-0410-abb6-a92efa0c2026>
2008-12-17 12:19:03 +0000

    change

---
+++ trunk/setup.py
@@ -0,0 +1,43 @@
+import uliweb
+from setuptools import setup, find_packages
+import sys, os
+from distutils import log
+
+setup(name='uliweb',
+      version=uliweb.version,
+      description="Easy python web framework",
+      long_description="""\
+Uliweb is a new brand Python Web Framework. Before creating this framework, 
+I've learned and used many frameworks, Karrigell, Cherrypy, Django, web2py, 
+but more or less they are not fully satisfy me. So I decide to create myself 
+web framework, I hope I can merge their advantages into Uliweb, and I'll try 
+to make it simple and easy to use.
+
+This project is created and leaded by Limodou <limodou@gmail.com>. And it has 
+received many helps from many kind people.""",
+      classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GPLv2 License",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+      ],
+      packages = find_packages(),
+      install_requires = ['WebOb', 'simplejson', 'sqlalchemy'],
+      platforms = 'any',
+      keywords='wsgi web framework',
+      author='limodou',
+      author_email='limodou@gmail.com',
+      url='http://code.google.com/p/uliweb/',
+      license='GPLv2',
+      include_package_data=True,
+      zip_safe=False,
+      entry_points = {
+          'console_scripts': [
+              'uliweb = uliweb.manage:main',
+          ],
+      },
+      
+      )
