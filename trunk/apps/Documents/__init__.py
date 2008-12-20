02d51c5c56b97723d048f5656a1e82ec1f456bdb
limodou.project <limodou.project@14287918-b64d-0410-abb6-a92efa0c2026>
2008-12-20 14:39:51 +0000

    change settings.py to settings.ini format

---
+++ trunk/apps/Documents/__init__.py
@@ -0,0 +1,17 @@
+from uliweb.core.plugin import plugin
+
+##################################################
+# insert rst2html function to template environment
+##################################################
+@plugin('prepare_template_env')
+def prepare_template_env(sender, env):
+    from uliweb.utils.rst import to_html
+    from uliweb.core.SimpleFrame import errorpage
+    def rst2html(filename):
+        f = env.get_file(filename)
+        if f:
+            return to_html(file(f).read())
+        else:
+            errorpage("Can't find the file %s" % filename)
+    env['rst2html'] = rst2html
+    
