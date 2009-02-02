8e90d39d150360d3363ad166edbabb58e7f348b5
limodou.project <limodou.project@14287918-b64d-0410-abb6-a92efa0c2026>
2009-02-02 14:48:05 +0000

    add one2one

--- trunk/test/test_orm.py
+++ trunk/test/test_orm.py
@@ -3,6 +3,7 @@ import time, sys
 sys.path.insert(0, '../uliweb/lib')
 from uliweb.orm import *
 
+#basic testing
 def test_1():
     """
     >>> set_auto_bind(True)
@@ -42,7 +43,7 @@ def test_1():
     0
     """
     
-
+#testing model alter one the fly
 def test_2():
     """
     >>> set_auto_bind(True)
@@ -59,6 +60,7 @@ def test_2():
     ['username', 'id', 'year']
     """
     
+#testing many2one
 def test_3():
     """
     >>> set_auto_bind(True)
@@ -92,6 +94,7 @@ def test_3():
     <Test {'username':'zoom','year':0,'id':1}>
     """
     
+#testing many2one using collection_name
 def test_4():
     """
     >>> set_auto_bind(True)
@@ -112,4 +115,59 @@ def test_4():
     <Test1 {'test':<Test {'username':'limodou1','year':0,'id':1}>,'name':'zoom','id':1}>
     >>> a1.tttt.count()
     2
+    >>> b3 = Test1(name='aaaa').save()
+    >>> a1.tttt.count()
+    2
+    >>> b3.test = a1
+    >>> b3.save()
+    <Test1 {'test':<Test {'username':'limodou1','year':0,'id':1}>,'name':'aaaa','id':3}>
+    >>> Test1.get(3)
+    <Test1 {'test':<Test {'username':'limodou1','year':0,'id':1}>,'name':'aaaa','id':3}>
+    """
+    
+#testing transaction
+def test_5():
+    """
+    >>> set_auto_bind(True)
+    >>> db = get_connection('sqlite://')
+    >>> db.metadata.drop_all()
+    >>> class Test(Model):
+    ...     username = Field(unicode)
+    ...     year = Field(int, default=0)
+    >>> t = db.begin()
+    >>> a = Test(username='limodou').save()
+    >>> b = Test(username='limodou').save()
+    >>> db.rollback()
+    >>> Test.count()
+    0
+    >>> t = db.begin()
+    >>> a = Test(username='limodou').save()
+    >>> b = Test(username='limodou').save()
+    >>> db.commit()
+    >>> Test.count()
+    2
+    """
+  
+#testing one2one
+def test_6():
+    """
+    >>> set_auto_bind(True)
+    >>> db = get_connection('sqlite://')
+    >>> db.metadata.drop_all()
+    >>> class Test(Model):
+    ...     username = Field(str)
+    ...     year = Field(int)
+    >>> class Test1(Model):
+    ...     test = One2One(Test)
+    ...     name = Field(str)
+    >>> a1 = Test(username='limodou1').save()
+    >>> b1 = Test1(name='zoom', test=a1).save()
+    >>> a1
+    <Test {'username':'limodou1','year':0,'id':1}>
+    >>> a1.test1
+    <Test1 {'test':<Test {'username':'limodou1','year':0,'id':1}>,'name':'zoom','id':1}>
+    >>> b1.test
+    <Test {'username':'limodou1','year':0,'id':1}>
+    >>> b1.test == a1
+    True
     """
