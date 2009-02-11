6237282bdfb0402f27753571a4ac301c4bc0b272
limodou.project <limodou.project@14287918-b64d-0410-abb6-a92efa0c2026>
2009-02-11 12:11:39 +0000

    add define() function to template, you can use it to detect if a var is existed, but you must pass the string name of the variable, I can't analysis the variable name from the source now. and add subfolder parameter to save_file().

--- uliweb/contrib/upload/__init__.py
+++ uliweb/contrib/upload/__init__.py
@@ -1,5 +1,4 @@
 import os
-from uliweb.utils.common import log
 from uliweb.core.plugin import plugin
 from uliweb.core.SimpleFrame import expose
 from werkzeug.exceptions import Forbidden
@@ -19,27 +18,30 @@ def file_serving(filename):
     fname = files.encoding_filename(fname, s.HTMLPAGE_ENCODING, s.FILESYSTEM_ENCODING)
     return return_file(fname)
     
-def _get_normal_filename(filename, path_to=None):
+def _get_normal_filename(filename, path_to=None, subfolder=''):
     path = path_to or application.settings.UPLOAD.TO_PATH
+    if subfolder:
+        path = os.path.join(path, subfolder)
     fname = os.path.normpath(filename)
     f = os.path.join(path, fname).replace('\\', '/')
     if not f.startswith(path):
         raise Forbidden("You can not visit unsafe files.")
     return f
 
-def save_file(filename, fobj, path_to=None, replace=False):
+def save_file(filename, fobj, path_to=None, replace=False, subfolder=''):
     from uliweb.utils import files
     assert hasattr(fobj, 'read'), "fobj parameter should be a file-like object"
-    fname = _get_normal_filename(filename, path_to)
+    fname = _get_normal_filename(filename, path_to, subfolder)
     s = application.settings.GLOBAL
     fname = files.encoding_filename(fname, s.HTMLPAGE_ENCODING, s.FILESYSTEM_ENCODING)
-    filename = files.save_file(fname, fobj, path_to, replace, application.settings.UPLOAD.BUFFER_SIZE)
+    
+    filename = files.save_file(fname, fobj, replace, application.settings.UPLOAD.BUFFER_SIZE)
     return files.encoding_filename(filename, s.FILESYSTEM_ENCODING, s.HTMLPAGE_ENCODING)
         
-def get_filename(filename, path_to=None):
-    return _get_normal_filename(filename, path_to)
+def get_filename(filename, path_to=None, subfolder=''):
+    return _get_normal_filename(filename, path_to, subfolder)
 
-def get_url(filename, path_to=None):
+def get_url(filename, path_to=None, subfolder=''):
     import urllib
     filename = urllib.quote_plus(filename)
-    return _get_normal_filename(filename, application.settings.UPLOAD.URL_SUFFIX)
+    return _get_normal_filename(filename, application.settings.UPLOAD.URL_SUFFIX, subfolder)
