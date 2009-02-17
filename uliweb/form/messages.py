8a7d6870553f6324fa85cd93acc0be81ecbfa7c8
limodou.project <limodou.project@14287918-b64d-0410-abb6-a92efa0c2026>
2009-02-17 01:26:48 +0000

    change documents and improve form module, add ImageField and IS_IMAGE validator

--- uliweb/form/messages.py
+++ uliweb/form/messages.py
@@ -4,3 +4,5 @@ unsupport_error = 'Unsupport type %s'
 convert_error = "Can't convert %r to %s."
 date_format_error = "The data is not a valid data format."
 time_format_error = "The data is not a valid time format."
+is_not_valid_image = "The file is not a valid image."
+image_size_is_to_large = "The image file size exceeds the limit."
--- uliweb/form/validators.py
+++ uliweb/form/validators.py
@@ -25,8 +25,22 @@ def IS_IN_SET(choices):
     '''
     choices should be a list or a tuple, e.g. [1,2,3]
     '''
-    def f(data):
+    def f(data, rquest=None):
         if data not in __get_choices_keys(choices):
             raise ValidationError, messages.is_in_set_validate_error
     return f
 
+def IS_IMAGE(size=None):
+    def f(data, request=None):
+        import Image
+        try:
+            try:
+                image = Image.open(data.file)
+                if size:
+                    if image.size[0]>size[0] or image.size[1]>size[1]:
+                        raise ValidationError, messages.image_size_is_to_large
+            except Exception, e:
+                raise ValidationError, messages.is_not_valid_image
+        finally:
+            data.file.seek(0)
+    return f
