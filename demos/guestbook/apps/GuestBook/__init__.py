820069070f9001119654fb95c80195c628c4eba0
limodou.project <limodou.project@14287918-b64d-0410-abb6-a92efa0c2026>
2009-02-22 07:37:12 +0000

    move guestbook to uliweb-tests project

--- demos/guestbook/apps/GuestBook/__init__.py
+++
@@ -1,6 +0,0 @@
-from uliweb.core.dispatch import bind
-
-@bind('prepare_template_env')
-def prepare_template_env(sender, env):
-    from uliweb.utils.textconvert import text2html
-    env['text2html'] = text2html
--- demos/guestbook/apps/GuestBook/models.py
+++
@@ -1,9 +0,0 @@
-from uliweb.orm import *
-import datetime
-
-class Note(Model):
-    username = Field(str)
-    message = Field(text)
-    homepage = Field(str)
-    email = Field(str)
-    datetime = Field(datetime.datetime, auto_now_add=True)
