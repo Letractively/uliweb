10faeb471433b0d1e799c24674a184e589f496f2
limodou.project <limodou.project@14287918-b64d-0410-abb6-a92efa0c2026>
2009-08-11 01:32:29 +0000

    add menu support

--- uliweb/contrib/admin/views.py
+++ uliweb/contrib/admin/views.py
@@ -1,15 +1,19 @@
 #coding=utf-8
 from uliweb.core.SimpleFrame import expose
+from uliweb.contrib.admin.menu import bind_menu
 
 @expose('/admin')
+@bind_menu('Settings', weight=10)
 def admin_index():
     return {}
 
 @expose('/admin/appsinfo')
+@bind_menu('Apps Info', weight=20)
 def admin_appsinfo():
     return {'apps':application.apps}
 
 @expose('/admin/urls')
+@bind_menu('Urls', weight=30)
 def admin_urls():
     u = []
     for r in application.url_map.iter_rules():
@@ -23,6 +27,7 @@ def admin_urls():
     return {'urls':u}
 
 @expose("/admin/global")
+@bind_menu('View Global', weight=40)
 def admin_globals():
 #    glob = globals()
 #    glo = [ (key,glob[key]) for key in glob.keys() if callable(glob[key]) ]
