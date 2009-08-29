0fc5633e2c82893c2a38d2d4b30ee42cf0c2bd48
limodou.project <limodou.project@14287918-b64d-0410-abb6-a92efa0c2026>
2009-08-29 09:27:02 +0000

    improve yaml, add base_layout.css and base_layout.html fix template, remove xmlescape function in Out class split uliform into several parts

--- trunk/uliweb/form/messages.py
+++
@@ -1,8 +0,0 @@
-choice_datatype_error = 'Choices need a dict, tuple or list data.'
-is_in_set_validate_error = 'Select a valid choice. That choice is not one of the available choices.'
-unsupport_error = 'Unsupport type %s'
-convert_error = "Can't convert %r to %s."
-date_format_error = "The data is not a valid data format."
-time_format_error = "The data is not a valid time format."
-is_not_valid_image = "The file is not a valid image."
-image_size_is_to_large = "The image file size exceeds the limit."
