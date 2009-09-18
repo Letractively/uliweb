4192077541ee48cee64b02b97cd57bd152eb75ae
limodou.project <limodou.project@14287918-b64d-0410-abb6-a92efa0c2026>
2009-09-18 13:52:03 +0000

    refactor, add web.py and conf.py

--- trunk/uliweb/core/conf.py
+++ trunk/uliweb/core/conf.py
@@ -1,2 +1,8 @@
 application = None
 settings = None
+urls = []
+static_views = []
+url_map = None
+local = None
+apps_dir = 'apps'
+use_urls = False
--- trunk/uliweb/core/rules.py
+++ trunk/uliweb/core/rules.py
@@ -1,5 +1,5 @@
 from werkzeug.routing import Map, Rule
-_static_views = []
+static_views = []
 
 def Mapping(**kwargs):
     return Map(**kwargs)
@@ -8,6 +8,6 @@ def add_rule(map, url, endpoint=None, **kwargs):
     kwargs['endpoint'] = endpoint
     static = kwargs.pop('static', None)
     if static:
-        _static_views.append(kwargs['endpoint'])
+        static_views.append(kwargs['endpoint'])
     map.add(Rule(url, **kwargs))
             
