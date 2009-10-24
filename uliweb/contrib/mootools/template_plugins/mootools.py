d265dce18788affebf33b228ebb702ed00076a75
limodou <limodou@14287918-b64d-0410-abb6-a92efa0c2026>
2009-10-24 13:13:42 +0000

    fix mootools app bug

--- uliweb/contrib/mootools/template_plugins/mootools.py
+++ uliweb/contrib/mootools/template_plugins/mootools.py
@@ -1,6 +1,10 @@
-def call(app, var, env, version='1.2.3', more=True):
+def call(app, var, env, version=None, more=True):
     a = []
-    a.append('{{=url_for_static("mootools/mootools-%s-core.js")}}' % version)
+    if version:
+        version = '-%s' % version
+    else:
+        version = ''
+    a.append('{{=url_for_static("mootools/mootools%s-core.js")}}' % version)
     if more:
-        a.append('{{=url_for_static("mootools/mootools-%s-more.js")}}' % version)
+        a.append('{{=url_for_static("mootools/mootools%s-more.js")}}' % version)
     return {'toplinks':a}
