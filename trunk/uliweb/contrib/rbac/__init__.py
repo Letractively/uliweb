8c6a03b94faed6c2321951539f718bb5f68d1982
limodou <limodou@14287918-b64d-0410-abb6-a92efa0c2026>
2010-06-12 14:20:28 +0000

    fix rbac bug

--- trunk/uliweb/contrib/rbac/__init__.py
+++ trunk/uliweb/contrib/rbac/__init__.py
@@ -1,1 +1,8 @@
 from rbac import *
+
+from uliweb.core.dispatch import bind, get
+
+@bind('prepare_default_env')
+def prepare_default_env(sender, env):
+    func = permissions.ok
+    env['permission'] = func
