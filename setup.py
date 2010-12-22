86f01fa27644f78833f7fb0fcbdd4c9cbd2135ff
limodou <limodou@14287918-b64d-0410-abb6-a92efa0c2026>
2010-12-22 13:33:45 +0000

    fix

--- setup.py
+++ setup.py
@@ -151,7 +151,7 @@ Goals
 """
 
 setup(name='Uliweb',
-      version='0.0.1a4',
+      version='0.0.1a5',
       description="Easy python web framework",
       long_description=__doc__,
       classifiers=[
