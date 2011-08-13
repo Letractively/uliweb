43fa87af0a03e060a4cd5fca3b799be18c0bae97
limodou <limodou@14287918-b64d-0410-abb6-a92efa0c2026>
2011-08-13 10:23:15 +0000

    refactor weto

--- trunk/uliweb/lib/weto/backends/database_storage.py
+++ trunk/uliweb/lib/weto/backends/database_storage.py
@@ -1,76 +1,49 @@
-import cPickle
-from base import BaseStorage, StorageError
+from base import BaseStorage, KeyError
+import time
 
-try:
-    import sqlalchemy as sa
-    from sqlalchemy import types
-except ImportError:
-    raise StorageError("Database cache backend requires the 'sqlalchemy' library")
+import sqlalchemy as sa
+from sqlalchemy import types
 
 class Storage(BaseStorage):
-    def __init__(self, options):
+    def __init__(self, cache_manager, options):
+        BaseStorage.__init__(self, cache_manager, options)
+
         self.url = options.get('url', 'sqlite:///')
         self.tablename = options['table_name']
         self.auto_create = options.get('auto_create', True)
         self.db, self.meta, self.table = create_table(self.url, self.tablename, self.auto_create)
         
-    def get_lock(self, key):
-        return True
+    def get(self, key):
+        result = sa.select([self.table.c.data, self.table.c.expiry_time,
+                            self.table.c.stored_time], 
+                           self.table.c.key==key,
+                          for_update=True).execute().fetchone()
+        if result:
+            if self._is_not_expiry(result['stored_time'], result['expiry_time']):
+                value = self._load(result['data'])
+                return value
+        raise KeyError, "Cache key [%s] not found" % key
     
-    def read_ready(self, key):
-        return True
-        
-    def acquire_read_lock(self, lock):
-        return self.db.begin()
-        
-    def release_read_lock(self, lock, success):
-        if success:
-            return self.db.commit()
+    def set(self, key, value, expire):
+
+        v = self._dump(value)
+        result = sa.select([self.table.c.data, self.table.c.id], 
+                           self.table.c.key==key,
+                          for_update=True).execute().fetchone()
+        now = time.time()
+        if result:
+            self.table.update(self.table.c.id==result['id']).execute(
+                data=v, stored_time=now, expiry_time=expire)
         else:
-            return self.db.rollback()
-        
-    def acquire_write_lock(self, lock):
-        return self.db.begin()
-        
-    def release_write_lock(self, lock, success):
-        if success:
-            return self.db.commit()
-        else:
-            return self.db.rollback()
-      
-    def load(self, key):
-        if key:
-            result = sa.select([self.table.c.data, self.table.c.expiry_time,
-                                self.table.c.stored_time], 
-                               self.table.c.key==key,
-                              for_update=True).execute().fetchone()
-            if result:
-                try:
-                    v = cPickle.loads(result['data'])
-                    return result['stored_time'], result['expiry_time'], v
-                except (IOError, OSError, EOFError, cPickle.PickleError, ValueError):
-                    pass
-                
-    
-    def save(self, key, stored_time, expiry_time, value, modified):
-        if key:
-            v = cPickle.dumps(value)
-            result = sa.select([self.table.c.data, self.table.c.id], 
-                               self.table.c.key==key,
-                              for_update=True).execute().fetchone()
-            if result:
-                self.table.update(self.table.c.id==result['id']).execute(
-                    data=v, stored_time=stored_time, expiry_time=expiry_time)
-            else:
-                self.table.insert().execute(key=key, data=v,
-                                   stored_time=stored_time, expiry_time=expiry_time)
+            self.table.insert().execute(key=key, data=v,
+                               stored_time=now, expiry_time=expire)
     
     def delete(self, key):
-        if key:
-            self.table.delete(self.table.c.key==key).execute()
+        self.table.delete(self.table.c.key==key).execute()
+        return True
             
-    def delete_lock(self, lock):
-        pass
+    def _is_not_expiry(self, accessed_time, expiry_time):
+        return time.time() < accessed_time + expiry_time
     
 def create_table(url, tablename, create=False):
     db = sa.create_engine(url, strategy='threadlocal')
--- trunk/uliweb/lib/weto/cache.py
+++ trunk/uliweb/lib/weto/cache.py
@@ -4,20 +4,10 @@
 # storage class will ensure the sync when load and save a session from 
 # and to the storage.
 #########################################################################
-import time
+import cPickle
+from backends.base import KeyError
 
-try:
-    from hashlib import md5
-except ImportError:
-    from md5 import md5
-
-def _get_key(key):
-    if isinstance(key, unicode):
-#        key = key.encode('ascii', 'backslashreplace')
-        key = key.encode('utf-8')
-    
-#    return md5(key).hexdigest()
-    return key
+__modules__ = {}
 
 def wrap_func(des, src):
     des.__name__ = src.__name__
@@ -27,101 +17,64 @@ def wrap_func(des, src):
     des.__dict__.update(src.__dict__)
     return des
 
-class CacheKeyException(Exception):pass
+class NoSerial(object):
+    def load(self, s):
+        return s
+    
+    def dump(self, v):
+        return v
+    
+class Serial(NoSerial):
+    def load(self, s):
+        return cPickle.loads(s)
+    
+    def dump(self, v):
+        return cPickle.dumps(v, cPickle.HIGHEST_PROTOCOL)
+    
+class Empty(object):
+    pass
 
 class Cache(object):
-    default_expiry_time = 3600*24*365
-    default_storage_type = 'file'
-    default_options = {'table_name':'uliweb_cache', 'data_dir':'./caches',
-        'file_dir':'./caches/cache_files',
-        'lock_dir':'./caches/cache_files_lock'}
-    
-    def __init__(self, storage_type=None, options=None, expiry_time=None):
-        self._modules = {}
-        self._storage_type = storage_type or self.default_storage_type
-        self._options = self.default_options
-        if options:
-            self._options.update(options.copy())
+    def __init__(self, storage_type='file', options=None, expiry_time=3600*24*365,
+        serial_cls=Serial):
+        self._storage_type = storage_type
+        self._options = options or {}
         self._storage_cls = self.__get_storage()
         self._storage = None
-        self.expiry_time = expiry_time or self.default_expiry_time
+        self._serial_cls = serial_cls
+        self.serial_obj = serial_cls()
+        self.expiry_time = expiry_time
      
     def __get_storage(self):
-        modname = 'weto.backends.%s_storage' % self._storage_type
-        if modname in self._modules:
-            return self._modules[modname]
+        modname = 'backends.%s_storage' % self._storage_type
+        if modname in __modules__:
+            return __modules__[modname]
         else:
             mod = __import__(modname, {}, {}, [''])
             _class = getattr(mod, 'Storage', None)
-            self._modules[modname] = _class
+            __modules__[modname] = _class
         return _class
     
     @property
     def storage(self):
         if not self._storage:
-            self._storage = self._storage_cls(self._options)
+            self._storage = self._storage_cls(self, self._options)
         return self._storage
     
-    def get(self, k=None, default=None):
-        key = _get_key(k)
-        if not self.storage.read_ready(key):
-            if default is None:
-                raise CacheKeyException, "Cache key [%s] not found" % k
-            return default
-            
-        lock = self.storage.get_lock(key)
+    def get(self, key, default=Empty):
         try:
-            self.storage.acquire_read_lock(lock)
-            ret = self.storage.load(key)
-            if ret:
-                #if ret is tuple or list, then just if the value is expired
-                if isinstance(ret, (tuple, list)):
-                    stored_time, expiry_time, value = ret
-                    if self._is_not_expiry(stored_time, expiry_time):
-                        return value
-                #or simple consider the value is not expired
-                else:
-                    return ret
-        except:
-            self.storage.release_read_lock(lock, False)
-        else:
-            self.storage.release_read_lock(lock, True)
-        if default is None:
-            raise CacheKeyException, "Cache key [%s] not found" % k
-        return default
+            return self.storage.get(key)
+        except KeyError, e:
+            if default is not Empty:
+                return default
+            else:
+                raise
             
-    def _is_not_expiry(self, accessed_time, expiry_time):
-        return time.time() < accessed_time + expiry_time
-        
     def set(self, key, value=None, expire=None):
-        if value is None:
-            return True
-        key = _get_key(key)
-        now = time.time()
-
-        lock = self.storage.get_lock(key)
-        try:
-            self.storage.acquire_write_lock(lock)
-            self.storage.save(key, now, expire or self.expiry_time, value, True)
-        except:
-            self.storage.release_write_lock(lock, False)
-            raise
-        else:
-            self.storage.release_write_lock(lock, True)
-            return True
+        return self.storage.set(key, value, expire or self.expiry_time)
         
     def delete(self, key):
-        key = _get_key(key)
-        lock = self.storage.get_lock(key)
-        try:
-            self.storage.acquire_write_lock(lock)
-            self.storage.delete(key)
-        except:
-            self.storage.release_write_lock(lock, False)
-            raise
-        else:
-            self.storage.release_write_lock(lock, True)
-            self.storage.delete_lock(lock)
+        return self.storage.delete(key)
              
     def __getitem__(self, key):
         return self.get(key)
@@ -136,8 +89,8 @@ class Cache(object):
         try:
             v = self.get(key)
             return v
-        except CacheKeyException:
-            self.set(key, defaultvalue, expire=expire)
+        except KeyError:
+            self.set(key, defaultvalue, expire)
             return defaultvalue
         
     def cache(self, k=None, expire=None):
@@ -151,7 +104,7 @@ class Cache(object):
                 try:
                     ret = self.get(key)
                     return ret
-                except CacheKeyException:
+                except KeyError:
                     ret = func(*args, **kwargs)
                     self.set(key, ret, expire=expire)
                     return ret
